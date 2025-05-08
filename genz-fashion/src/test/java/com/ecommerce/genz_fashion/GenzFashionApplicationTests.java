9b6d02da77c49711f5fad49b36916946695b3913
tainxDev <tainguyenxuan287@gmail.com>
2025-05-08 14:56:07 +0700

    add project spring.io
    tạo dự án từ spring intializr
    chuyển thư mục vào folder github đã tạo
    done

---
+++ genz-fashion/src/test/java/com/ecommerce/genz_fashion/GenzFashionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ecommerce.genz_fashion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GenzFashionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
